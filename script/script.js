7fa5e487cd3167fac021b1a90d889e1850ccd8e7
Boris Tsarfin <legirovanniy@mail.ru>
2020-11-22 00:29:41 +0400

    First commit

---
+++ script/script.js
@@ -0,0 +1,16 @@
+
+
+// объявление переменных
+
+let money;
+let income;
+let addExpenses;
+let deposit;
+let miccion;
+let period; 
+
+//вывод сообщения в модельном окне
+alert('Да прибудет с нами сила!');
+
+//вывод сообщения в консоль
+console.log (111222333); 
