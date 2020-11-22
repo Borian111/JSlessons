17696ab2fd10d96bae5b4f7a9a240b288584adcb
Boris Tsarfin <legirovanniy@mail.ru>
2020-11-23 01:20:38 +0400

    2

--- lesson2/script/script.js
+++ lesson2/script/script.js
@@ -2,15 +2,43 @@
 
 // объявление переменных
 
-let money;
-let income;
-let addExpenses;
-let deposit;
-let miccion;
-let period; 
+let money = 130000 //любое число “Доход за месяц”,
 
-//вывод сообщения в модельном окне
-alert('Да прибудет с нами сила!');
+let income = 50000 //строка с дополнительными доходом (например: фриланс), 
 
-//вывод сообщения в консоль
-console.log (111222333); 
+let addExpenses = 'Interet,Taxi,Tax' // строка с перечислением дополнительных расходов через запятую (например: интернет, такси, коммуналка), 
+
+let deposit = true // любое булево значение,
+
+let mission = 20000000 //любое число (Какую сумму хотите накопить),
+
+let period = 12 // число от 1 до 12 (месяцев)
+
+
+// Вывести в консоль тип данных значений переменных money, income, deposit;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+//Вывести в консоль длину строки addExpenses
+
+console.log(addExpenses.length);
+
+//Вывести в консоль “Период равен (period) месяцев” и “Цель заработать (mission) рублей/долларов/гривен/юани”
+
+console.log(' период равен ',period," месяцев \n",'Цель заработать ', mission," рублей");
+
+
+
+//Привести строку addExpenses к нижнему регистру и разбить строку на массив, вывести массив в консоль
+addExpenses = addExpenses.toLowerCase();
+console.log(addExpenses.split(','));
+
+
+
+//Объявить переменную budgetDay и присвоить дневной бюджет (доход за месяц / 30)
+let budgetDay = (money+income)/30;
+//Вывести в консоль budgetDay
+console.log(budgetDay);
+
