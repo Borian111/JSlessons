56b538f4bd2b7c849f23540d3e7e3950acec6f16
Boris Tsarfin <legirovanniy@mail.ru>
2020-12-19 03:47:34 +0400

    fine12

--- script/script.js
+++ script/script.js
@@ -1,41 +1,106 @@
 'use strict'
-const todoControl = document.getElementsByClassName('todo-control');
-const headerInput = document.getElementsByClassName('header-input');
+const todoControl = document.querySelector('.todo-control');
+const headerInput = document.querySelector('.header-input');
 const todoList = document.querySelector('.todo-list');
 const todoCompleted = document.querySelector('.todo-completed');
 console.log(todoControl);
 console.log(headerInput);
-const todoData = [
-    {
-         value: 'Сварить кофе',
-         completed: false
-    },
-    {
-        value: 'Помыть посуду',
-        completed: true
-
-    }
+let todoData = [];
 
 
-];
+
+
+
+
+
 const render = function(){
+       todoList.textContent = '';
+       todoCompleted.textContent = '';
+       
+      
+      
+       
     todoData.forEach(function(item){
        const li = document.createElement('li');
        li.classList.add('todo-item');
        
-       li.innerHTML = '<span class="text-todo">Сварить кофе</span>'
-        console.log(item);
+       li.innerHTML = '<span class="text-todo">' + item.value + '</span>'+
+            '<div class="todo-buttons">' + 
+            '<button class="todo-remove"></button>' +
+            '<button class="todo-complete"></button>' +
+            '</div>';
+             
+            if(item.completed){
+                todoCompleted.append(li);
+                 } else{
+                     todoList.append(li);
+                 }
+
+                       
+            const btntodoCompleted = li.querySelector('.todo-complete');
+            btntodoCompleted.addEventListener('click', function(){     
+            item.completed = !item.completed;
+            localStorage.setItem('todo', JSON.stringify(todoData));
+                  render();
+                });
+
+            const btntodoRemove = li.querySelector(".todo-remove");
+
+               btntodoRemove.addEventListener('click', function() {
+                  let i=todoData.indexOf(item);
+
+                  if(i>= 0){
+                      todoData.splice(i,1);
+                  }
+            
+            localStorage.setItem('todo', JSON.stringify(todoData));
+              
+           
+
+
+            render();
+            });
+
+
     });
+
+
 };
 
+
+
+
 todoControl.addEventListener('submit', function(event){
-     event.preventDefault();
+     event.preventDefault() 
+    });
+
+
+
+    todoControl.addEventListener('submit', function(){
+
+     if(headerInput.value ==='') {
+        alert('Заполните поле!')
+     }
+      else {  
      const newTodo = {
         value: headerInput.value,
         completed: false
 
-     };  
-     todoData.push(newTodo);
+     }; 
+      
+       todoData.push(newTodo);
+       localStorage.setItem('todo', JSON.stringify(todoData)); 
+       render();
+       headerInput.value ='';
 
-     render();
-});
+    }
+      });
+         
+      if(localStorage.getItem('todo')) {
+   
+        todoData = JSON.parse(localStorage.getItem('todo'));
+        render();
+       }    
+      
+    
+render();
