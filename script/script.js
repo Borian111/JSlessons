d30fb3195f13639f8bab4a2c17e9f9e950b6c28f
Boris Tsarfin <legirovanniy@mail.ru>
2021-02-04 01:53:53 +0400

    les3upd

--- script/script.js
+++ script/script.js
@@ -1,8 +1,9 @@
 'use strict'
-let income = parseInt(50000);
-let mission = parseInt(20000000);
+let income = 'фриланс';
+let mission = parseInt(2000000);
+let period = 12 // число от 1 до 12 (месяцев)
 //Спрашиваем у пользователя “Ваш месячный доход?” и результат сохраняем в переменную money
-let money = prompt('Ваш месячный доход?');
+let money = parseInt(prompt('Ваш месячный доход?'));
 
 /*Спросить у пользователя “Перечислите возможные расходы за рассчитываемый период через запятую”
 сохранить в переменную addExpenses (пример: "Квартплата, проездной, кредит")*/
@@ -10,7 +11,7 @@ let addExpenses = prompt('Перечислите возможные расход
 
 /*Спросить у пользователя “Есть ли у вас депозит в банке?”
 и сохранить данные в переменной deposit (булево значение true/false)*/
-let deposit =prompt('Есть ли у вас депозит в банке?');
+let deposit =confirm('Есть ли у вас депозит в банке?');
 
 /*Спросить у пользователя по 2 раза каждый вопрос и записать ответы в разные переменные 
 
@@ -31,17 +32,17 @@ let aMount2 =prompt('Во сколько это обойдется?');
 /*Вычислить бюджет на месяц, учитывая обязательные расходы,
  сохранить в новую переменную budgetMonth и вывести результат в консоль*/
 
- let budgetMonth = parseInt(money) + income - parseInt(aMount1) - parseInt(aMount2);
+ let budgetMonth = money - aMount1 - aMount2;
  console.log('Бюджет на месяц', budgetMonth);
  /*Зная budgetMonth, посчитать за сколько месяцев будет достигнута цель mission,
   вывести в консоль, округляя в большую сторону*/
- let xMission = mission /((parseInt(money) + income) - budgetMonth);
+ let xMission = mission /(money - budgetMonth);
   console.log('Цель будет достигнута за ',Math.ceil(xMission),' месяцев');
 
 
 
 
-let period = 12 // число от 1 до 12 (месяцев)
+
 
 
 //Объявить переменную budgetDay и присвоить дневной бюджет (доход за месяц / 30)
@@ -49,9 +50,9 @@ let budgetDay = (budgetMonth)/30;
 //Вывести в консоль budgetDay
 console.log('бюджет на день', Math.floor(budgetDay));
 
-if (budgetDay => 1200) {
+if (budgetDay > 1200) {
     console.log('У вас высокий уровень дохода');
-} else if (600 <= budgetDay < 1200){
+} else if (600 <= budgetDay <= 1200){
     console.log('У вас средний уровень дохода');
 } else if (0 <= budgetDay < 600){
     console.log('У вас низкий уровень дохода');
